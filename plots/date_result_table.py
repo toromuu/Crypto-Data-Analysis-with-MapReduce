87e16e1be172d121619378c72d0050697a8d93f0
Mariano <mariano.serpis@gmail.com>
2022-05-22 22:48:15 +0200

    [ADD] Script para mostrar los datos en forma de tabla.

---
+++ plots/date_result_table.py
@@ -0,0 +1,33 @@
+import argparse
+from prettytable import PrettyTable
+
+if __name__ == '__main__':
+    labels = []
+    values = []
+    dates = []
+    lab_val = []
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="The filename to be processed")
+    args = parser.parse_args()
+
+    if args.filename:
+        with open(args.filename) as f:
+            for line in f:
+                label, value, date = line.strip().split("\t")
+                lab_val.append((label, float(value), date))
+
+    lab_val.sort(key=lambda x: x[1])
+    lab_val.reverse()
+
+    for i in range(len(lab_val)):
+        labels.append(lab_val[i][0])
+        values.append(lab_val[i][1])
+        dates.append(lab_val[i][2])
+
+    table = PrettyTable(['Moneda', 'Valor', 'Fecha'])
+    for i in range(len(labels)):
+        table.add_row([labels[i], values[i], dates[i]])
+
+    with open('query5_s3.txt', 'w') as f:
+        f.write(table.get_string())
